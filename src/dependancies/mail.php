52b54804015bbee0defe7c617ed8d74ea7bc6c90
Julian Wandhoven <Julian.wandhoven@gmail.com>
2023-06-04 20:16:36 +0200

    vendor localization problem

--- src/dependancies/mail.php
+++ src/dependancies/mail.php
@@ -1,5 +1,5 @@
 <?php
-    require_once __DIR__ . '/../../vendor/autoload.php';
+    require_once 'vendor/autoload.php';
     require_once __DIR__ . "/templates/email_verification.php";
 
     use SendGrid\Mail\Mail;
